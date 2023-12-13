76ef54765087eff91d946dcd6ba2f904944fa266
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-12-13 01:44:22 +0100

    contact form real

--- src/components/contactComp.vue
+++ src/components/contactComp.vue
@@ -1,6 +1,27 @@
 <template>
     <div id="contactCompDiv">
-
+        <div id="contactDiv">
+			<div id="titleDiv">
+				<v-icon icon="fas fa-envelope" />
+				<h2>
+					{{ $t('contact.contact') }}
+				</h2>
+			</div>
+            <form action="#" method="post" id="contactFormContainer">
+                <div id="leftFormDiv">
+                    <input name="Email" id="email" class="inputText" :placeholder="$t('contact.lastName')" required>
+                    <input name="Email" id="email" class="inputText" :placeholder="$t('contact.firstName')" required>
+                    <input name="Email" id="email" class="inputText" :placeholder="$t('contact.mail')" required>
+                    <input name="Email" id="email" class="inputText" :placeholder="$t('contact.phone')">
+                    <textarea name="yourMessage" id="yourMessageTextArea" :placeholder="$t('contact.message')" required></textarea>
+                </div>
+                <div id="sendButtonDiv">
+                    <button type="submit" id="sendButton">
+                        Send
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -13,7 +34,88 @@ export default {
 <style scoped>
 
 #contactCompDiv {
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 75vh;
 }
 
+#contactDiv {
+	width: 90vw;
+	height: 60vh;
+}
+
+#titleDiv {
+	display: flex;
+	align-items: center;
+	height: 8vh;
+    margin-bottom: 4vh;
+}
+
+h2 {
+	margin-left: 2vw;
+}
+
+#contactFormContainer {
+    height: 44vh;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+        justify-content: center;
+}
+
+#leftFormDiv {
+    width: 66vw;
+}
+
+.inputText {
+    width: 32vw;
+    height: 6vh;
+    padding: 1vh; 
+    margin: 0.3vh;
+    background-color: #F8F6F2;
+    color: #264097;
+}
+
+#yourMessageTextArea {
+    height: 30vh;
+    width: 64.3vw;
+    padding: 1vh; 
+    margin: 0.3vh;
+    background-color: #F8F6F2;
+    color: #264097;
+}
+
+#sendButtonDiv {
+    width: 10vw;
+    display: flex;
+    align-items: end;
+}
+
+#sendButton {
+    height: 8vh;
+    width: 9vw;
+    margin: 0.2vh;
+    background-color: #F8F6F2;
+    color: #3B57B1;
+}
+
+@media (max-width: 1199px) {
+    .inputText {
+        width: 64.3vw;
+    }
+
+    #sendButtonDiv {
+        width: 66vw;
+        justify-content: end;
+    }
+
+    #sendButton {
+        min-width: 100px;
+    }
+
+    #contactFormContainer {
+        justify-content: center;
+    }
+}
 </style>
